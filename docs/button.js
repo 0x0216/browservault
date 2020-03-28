b9f11d95de4773309ec5db3c9462431b76d136f5
3Axes <3Axesofficial@gmail.com>
2020-03-28 17:31:34 -0400

    Update button.js

--- docs/button.js
+++ docs/button.js
@@ -8,7 +8,7 @@ body.appendChild(button);
 
 button.addEventListener ("click", function() {
   alert("You have caught deez nuts");
-  window.open("console");
+  window.open("creator.html");
 });
 
 $('#msg').html(data.message)
