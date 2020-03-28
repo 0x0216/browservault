c7dcf7f899dbcd9bb3030d665a34aeb0cc65978b
LFlamingice <60633693+LFlamingice@users.noreply.github.com>
2020-03-28 15:13:23 -0400

    button deez nuts

---
+++ src/button.js
@@ -0,0 +1,14 @@
+
+var button = document.createElement("button");
+button.innerHTML = "+ Create New";
+
+
+var body = document.getElementsByTagName("body")[0];
+body.appendChild(button);
+
+button.addEventListener ("click", function() {
+  alert("You have caught deez nuts");
+});
+$('#msg').html(data.message)
+
+console.log(data)
