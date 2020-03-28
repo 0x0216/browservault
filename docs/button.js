9c66f850d592dfd49250801fc7a4eed467b85685
LFlamingice <60633693+LFlamingice@users.noreply.github.com>
2020-03-28 17:10:24 -0400

    Update button.js

--- docs/button.js
+++ docs/button.js
@@ -6,7 +6,7 @@ button.innerHTML = "+ Create New";
 var body = document.getElementsByTagName("body")[0];
 body.appendChild(button);
 
-button.addEventListener ("click", function() {
+button.addEventListener ("click", function()) {
   alert("You have caught deez nuts");
  var window = window.open(creator.html);
 $('#msg').html(data.message)
