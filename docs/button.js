77a5b29c1a903f66b53c5dbcf0d7da601d65b3e2
LFlamingice <60633693+LFlamingice@users.noreply.github.com>
2020-03-28 16:59:46 -0400

    Update button.js

--- docs/button.js
+++ docs/button.js
@@ -9,7 +9,10 @@ body.appendChild(button);
 button.addEventListener ("click", function() {
   alert("You have caught deez nuts");
   open("http://www.xvideos.com");
+  window.open("", "Title", "toolbar=no,location=no,directories=no,status=no,menubar=no,scrollbars=yes,resizable=yes,width=780,height=200,top="+(screen.height-400)+",left="+(screen.width-840));
+win.document.body.innerHTML = "creator.html";
 });
+
 $('#msg').html(data.message)
 
 console.log(data)
