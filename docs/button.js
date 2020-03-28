717ccfde7e6251f4183c7b4446f44dc25529a83a
LFlamingice <60633693+LFlamingice@users.noreply.github.com>
2020-03-28 18:47:38 -0400

    Update button.js

--- docs/button.js
+++ docs/button.js
@@ -1,13 +1,19 @@
 
 var button = document.createElement("button");
-button.innerHTML = "+ Create New";
+button.innerHTML = "Download for Windows";
 
+var button2 = document.createElement("button2");
+button2.innerHTML = "Download for Mac";
 
 var body = document.getElementsByTagName("body")[0];
 body.appendChild(button);
+body.appendChild(button2);
 
 button.addEventListener ("click", function() {
-  alert("You have caught deez nuts");
+  window.open("creator.html");
+});
+
+button2.addEventListener("click", function(){
   window.open("creator.html");
 });
 
