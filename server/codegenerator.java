01bfdca20b414d6e47ab42d949f56a0dbf2c9e65
LFlamingice <60633693+LFlamingice@users.noreply.github.com>
2020-03-29 14:27:09 -0400

    Update codegenerator.java

--- server/codegenerator.java
+++ server/codegenerator.java
@@ -1,14 +1,27 @@
+import java.io.FileWriter;
+import java.io.File;
+import java.io.PrintWriter;
+import java.io.IOException;
 
 public class codegenerator {
     private static final String master = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()";
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
        double count = Math.random()*4 + 12;
        StringBuilder bruh = new StringBuilder();
        for (int i =0; i < count; i++){
            int character = (int)(Math.random()*master.length());
            bruh.append(master.charAt(character));
        }
-        System.out.println(bruh);
+        filer(bruh);
+        
+    }
+    public static void filer(StringBuilder s) throws IOException{
+        File file = new File("C:\\" +s.toString() + ".txt");
+        FileWriter br = new FileWriter("C:\\" +s.toString() + ".txt");
+        String su = s.toString();
+        br.write(su);
+        
     }
 
+    
 }
